73b35d8a8bd67b23832caf1efc7c626069f6424e
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-11 21:37:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'coupon/coupon_code:string'
-
-  get 'coupon/store:string'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
