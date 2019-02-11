a324fbaa593c02221436c1642bd60d23162f1944
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-11 22:01:26 +0000

    Automatically backed up by Learn

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -14,6 +14,8 @@ class CouponController < ApplicationController
   def create
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
-    @coupon.store
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
   end
 end
